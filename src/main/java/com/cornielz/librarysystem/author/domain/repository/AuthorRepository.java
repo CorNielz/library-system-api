16c1644a26e059e4b23d126b2b8b384df8fd96b3
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-03 16:24:17 -0300

    Basic author repository interface created

---
+++ src/main/java/com/cornielz/librarysystem/author/domain/repository/AuthorRepository.java
@@ -0,0 +1,13 @@
+package com.cornielz.librarysystem.domain.author;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AuthorRepository {
+    Optional<Author> findById(UUID id);
+    List<Author> findAll();
+
+    void save(Author author);
+    void deleteById(UUID id);
+}
