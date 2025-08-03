217a44c6b2f6d0ce877191af4ab411df4976851a
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-03 17:18:16 -0300

    Base reservation model structure

---
+++ src/main/java/com/cornielz/librarysystem/reservation/domain/model/Reservation.java
@@ -0,0 +1,36 @@
+package com.cornielz.librarysystem.domain.reservation;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Reservation {
+    private final UUID id;
+
+    private UUID userId;
+    private UUID bookId;
+
+    private LocalDateTime borrowingDate;
+    private LocalDateTime expectedReturnDate;
+    private LocalDateTime returnDate;
+
+    private BigDecimal appliedPrice;
+
+    private ReservationStatus status;
+
+    public Reservation(UUID id, UUID userId, UUID bookId, LocalDateTime borrowingDate, LocalDateTime expectedReturnDate, LocalDateTime returnDate, BigDecimal appliedPrice, ReservationStatus status) {
+        this.id = id;
+
+        this.userId = userId;
+        this.bookId = bookId;
+
+        this.borrowingDate = borrowingDate;
+        this.expectedReturnDate = expectedReturnDate;
+        this.returnDate = returnDate;
+
+        this.appliedPrice = appliedPrice;
+
+        this.status = status;
+    }
+}
