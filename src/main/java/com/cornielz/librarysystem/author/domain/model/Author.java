b994457cd51974896f0544377b09ba478013368e
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-03 16:14:26 -0300

    Author setters defined

--- src/main/java/com/cornielz/librarysystem/author/domain/model/Author.java
+++ src/main/java/com/cornielz/librarysystem/author/domain/model/Author.java
@@ -1,7 +1,7 @@
 package com.cornielz.librarysystem.domain.author;
 
 import java.util.Objects;
-import java.time.LocalDateTime
+import java.time.LocalDateTime;
 
 public class Author {
     private final UUID id;
@@ -9,21 +9,36 @@ public class Author {
     private String name;
     private String bios;
 
-    private LocalDateTime birthDdate;
+    private LocalDateTime birthDate;
     private String nationality;
 
-    public Author(UUID id, String name, String bios, LocalDateTime birthDdate, String nationality) {
+    public Author(UUID id, String name, String bios, LocalDateTime birthDate, String nationality) {
         this.id = id;
 
         this.name = name;
         this.bios = bios;
 
-        this.birthDdate = birthDdate;
+        this.birthDate = birthDate;
         this.nationality = nationality;
     }
 
     // Setters
 
+    public void rename(String newName){
+        this.name = newName;
+    }
+
+    public void rewriteBios(String newBios){
+        this.bios = newBios;
+    }
+
+    public void changeBirthDate(LocalDateTime newBirthDate){
+        this.birthDate = newBirthDate;
+    }
+
+    public void changeNationality(String newNationality){
+        this.nationality = newNationality;
+    }
 
     // Getters
 
@@ -39,8 +54,8 @@ public class Author {
         return this.bios;
     }
 
-    public LocalDateTime getBirthDdate() {
-        return this.birthDdate;
+    public LocalDateTime getBirthDate() {
+        return this.birthDate;
     }
 
     public String getNationality() {
