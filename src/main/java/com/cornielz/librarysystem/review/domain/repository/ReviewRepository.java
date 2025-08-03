1b7d78fcbe1acdb7e1155be29b7d8900a6d4dccb
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-03 18:07:43 -0300

    Basic review repository interface created

---
+++ src/main/java/com/cornielz/librarysystem/review/domain/repository/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.cornielz.librarysystem.domain.review;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ReviewRepository {
+    Optional<Review> findById(UUID id);
+    List<Review> findAllByUserId(UUID id);
+    List<Review> findAllByBookId(UUID id);
+    List<Review> findAll();
+
+    void save(Review review);
+    void delete(UUID id);
+}
