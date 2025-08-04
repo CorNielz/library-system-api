d829196bbaff74385b61b6edaaf5e7aa0ef30e9a
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-04 13:41:17 -0300

    Reservation response DTO

---
+++ src/main/java/com/cornielz/librarysystem/reservation/application/dto/ReservationResponseDTO.java
@@ -0,0 +1,17 @@
+package com.cornielz.librarysystem.application.reservation;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import com.cornielz.librarysystem.domain.reservation.ReservationStatus;
+
+public record ReservationResponseDTO(
+        UUID id,
+        UUID userId,
+        UUID bookId,
+        LocalDateTime borrowingDate,
+        LocalDateTime expectedReturnDate,
+        LocalDateTime returnDate,
+        BigDecimal appliedPrice,
+        ReservationStatus status
+) {}
