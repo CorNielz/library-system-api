374253ee1b799f0fce3fd966d08efdf9e9b7a4e7
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-04 07:21:22 -0300

    JPA entity getters and setters

--- src/main/java/com/cornielz/librarysystem/author/infrastructure/persistence/AuthorEntity.java
+++ src/main/java/com/cornielz/librarysystem/author/infrastructure/persistence/AuthorEntity.java
@@ -44,4 +44,45 @@ public class AuthorEntity {
     public void preUpdate() {
         this.lastUpdateAt = LocalDateTime.now();
     }
+
+
+    // Setters
+
+    public void rename(String newName){
+        this.name = newName;
+    }
+
+    public void rewriteBios(String newBios){
+        this.bios = newBios;
+    }
+
+    public void changeBirthDate(LocalDateTime newBirthDate){
+        this.birthDate = newBirthDate;
+    }
+
+    public void changeNationality(String newNationality){
+        this.nationality = newNationality;
+    }
+
+    // Getters
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBios() {
+        return this.bios;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return this.birthDate;
+    }
+
+    public String getNationality() {
+        return this.nationality;
+    }
 }
