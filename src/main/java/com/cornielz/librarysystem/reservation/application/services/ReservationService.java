ebc875e468bf99ccf3a0755928eb4ccb80c9e4fd
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-04 15:43:36 -0300

    Reservation services

---
+++ src/main/java/com/cornielz/librarysystem/reservation/application/services/ReservationService.java
@@ -0,0 +1,14 @@
+package com.cornielz.librarysystem.application.reservation;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ReservationService {
+    ReservationResponseDTO create(ReservationCreationRequestDTO request);
+    ReservationResponseDTO update(ReservationUpdateRequestDTO request);
+    void delete(UUID id);
+    ReservationResponseDTO getById(UUID id);
+    List<ReservationResponseDTO> listAll();
+    List<ReservationResponseDTO> listByUserId(UUID userId);
+    List<ReservationResponseDTO> listByBookId(UUID bookId);
+}
