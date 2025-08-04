c10fb9dd9aa438e458eac5fddd44ca0125612faa
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-04 08:44:04 -0300

    Correction on package and import

--- src/main/java/com/cornielz/librarysystem/author/infrastructure/repository/AuthorJpaRepository.java
+++ src/main/java/com/cornielz/librarysystem/author/infrastructure/repository/AuthorJpaRepository.java
@@ -1,8 +1,9 @@
-package com.cornielz.librarysystem.infrastructure.author.AuthorEntity;
+package com.cornielz.librarysystem.infrastructure.author;
 
-import com.cornielz.librarysystem.domain.author;
+import com.cornielz.librarysystem.domain.author.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.UUID;
 
 public interface SpringDataAuthorRepository extends JpaRepository<AuthorEntity, UUID> {
 
