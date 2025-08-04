b84e293fc060e03f9c2128608f21c5adae163419
Cornielz <danielhenrique.vieira@protonmail.com>
2025-08-04 15:54:23 -0300

    Book controller for web requests

---
+++ src/main/java/com/cornielz/librarysystem/book/api/controller/BookControler.java
@@ -0,0 +1,33 @@
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<BookResponseDTO> createBook(@Valid @RequestBody BookCreationRequestDTO dto) {
+        return ResponseEntity.ok(bookService.create(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookResponseDTO> updateBook(@PathVariable UUID id, @Valid @RequestBody BookUpdateRequestDTO dto) {
+        return ResponseEntity.ok(bookService.update(id, dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookResponseDTO> getBook(@PathVariable UUID id) {
+        return ResponseEntity.ok(bookService.getById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookResponseDTO>> listBooks() {
+        return ResponseEntity.ok(bookService.getAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable UUID id) {
+        bookService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
